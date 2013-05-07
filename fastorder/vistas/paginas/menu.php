4dc7f7d40980c89c81901d4644f16c1c38f0e6e6
colab_01.diem <runtim3.error@gmail.com>
2013-05-07 11:47:19 -0600

    menu

--- fastorder/vistas/paginas/menu.php
+++ fastorder/vistas/paginas/menu.php
@@ -23,51 +23,53 @@
 </style>
 
 <div id="menuPrincipal" style="position: absolute; width: 700px;margin-left: 20px;z-index: 6;background-color: white;display: block;right: 0px;top: 35px; display:none; ">
-	<div id="accordion">
-		<?php 
-			if ( $_SESSION['userInfo']['rol']==1 || $_SESSION['userInfo']['rol']==2 || $_SESSION['userInfo']['rol']==3 ){
-		?>
-		<h3>Reportes</h3>
-		<div class="submenu">
-			<a tablink="true" href="/reportes/vendidos" titulo="Vendidos" class="link">
-				<img src="http://png.findicons.com/files/icons/2389/web_icon_pack/65/bluestyle_09_star.png">		 
-				<div>Vendidos</div>
-			</a>
-			<a tablink="true" href="/reportes/top20" titulo="Top 20" class="link">
-				<img src="http://png.findicons.com/files/icons/2389/web_icon_pack/65/greenstyle_09_star.png">		 
-				<div>Top 20</div>
-			</a>
-			<a tablink="true" href="/reportes/ultimos20" titulo="Ultimos 20" class="link">
-				<img src="http://png.findicons.com/files/icons/2389/web_icon_pack/65/redstyle_09_star.png">		 
-				<div>Ultimos 20</div>
-			</a>
-			<a tablink="true" href="/reportes/novendidos" titulo="No Vendidos" class="link">
-				<img src="http://png.findicons.com/files/icons/2389/web_icon_pack/65/blackstyle_09_star.png">		          
-				<div>No vendidos</div>
-			</a>				
-		</div>
-		
-		
-		<?php
-			} 
-		?>
+	<div id="accordion">		
 		<?php 
 			if ( $_SESSION['userInfo']['rol']==1 || $_SESSION['userInfo']['rol']==2  ){
 		?>
-		<h3>Catalogos</h3>
-		<div class="submenu">
-			<a tablink="true" href="/tiendas/busqueda" titulo="Tiendas" class="link">
-				<img src="http://png.findicons.com/files/icons/2379/shopping/64/roadside_shop.png">		 
-				<div>Tiendas</div>
+		<h3>Documentos</h3>		 
+		<div class="submenu">			
+			<a tablink="true" href="/pedidosinternos/busqueda" titulo="Grupos" class="link">
+				<img src="http://png.findicons.com/files/icons/566/isimple_system/64/paste.png">		 
+				<div>Pedido Interno</div>
 			</a>				
-			<a tablink="true" href="/grupos/busqueda" titulo="Grupos" class="link">
-				<img src="http://png.findicons.com/files/icons/2354/dusseldorf/32/category.png">		 
-				<div>Grupos</div>
+			<a tablink="true" href="/ordenescompra/busqueda" titulo="Tiendas" class="link">
+				<img src="http://png.findicons.com/files/icons/566/isimple_system/64/shopping_cart.png">		 
+				<div>Orden de Compra</div>
+			</a>				
+			<a tablink="true" href="/commpras/busqueda" titulo="Tiendas" class="link">
+				<img src="http://png.findicons.com/files/icons/566/isimple_system/64/shopping_cart_full.png">		 
+				<div>Compra</div>
+			</a>				
+		</div>
+		<h3>Catalogos</h3>		 
+		<div class="submenu">			
+			<a tablink="true" href="/conf_serie/busqueda" titulo="Grupos" class="link">
+				<img src="http://png.findicons.com/files/icons/2580/android_icons/72/serial_number.png">		 
+				<div>Configuraci&oacute;n de series.</div>
+			</a>				
+			<a tablink="true" href="/articulodetalle/busqueda" titulo="Grupos" class="link">
+				<img src="http://png.findicons.com/files/icons/1226/agua_extras_vol_1/48/recipes_folder_badged.png">		 
+				<div>Receta de insumo</div>
 			</a>				
 			<a tablink="true" href="/articulos/busqueda" titulo="Articulos" class="link">
-				<img src="http://png.findicons.com/files/icons/1673/diagram_part_2/64/diagram_v2_28.png">		 
+				<img src="http://png.findicons.com/files/icons/1673/diagram_part_2/64/diagram_v2_17.png">		 
 				<div>Articulos</div>
 			</a>			
+			<a tablink="true" href="/almacenes/busqueda" titulo="Articulos" class="link">
+				<img src="http://png.findicons.com/files/icons/2579/iphone_icons/40/warehouse.png">		 
+				<div>Almacenes</div>
+			</a>			
+			<a tablink="true" href="/tiendas/busqueda" titulo="Tiendas" class="link">
+				<img src="http://png.findicons.com/files/icons/2018/business_icons_for/48/company.png">		 
+				<div>Sucursales</div>
+			</a>		
+			<a tablink="true" href="/tiendas/busqueda" titulo="Tiendas" class="link">
+				<img src="http://png.findicons.com/files/icons/2257/berlin/32/suppliers.png">		 
+				<div>Proveedores</div>
+			</a>		
+			
+			
 		</div>
 		
 		<h3>Sistema</h3>
--- portal/vistas/paginas/menu.php
+++ portal/vistas/paginas/menu.php
@@ -23,46 +23,31 @@
 </style>
 
 <div id="menuPrincipal" style="position: absolute; width: 700px;margin-left: 20px;z-index: 6;background-color: white;display: block;right: 0px;top: 35px; display:none; ">
-	<div id="accordion">
-		<?php 
-			if ( $_SESSION['userInfo']['rol']==1 || $_SESSION['userInfo']['rol']==2 || $_SESSION['userInfo']['rol']==3 ){
-		?>
-		<h3>Reportes</h3>
-		<div class="submenu">
-			<a tablink="true" href="/reportes/vendidos" titulo="Vendidos" class="link">
-				<img src="http://png.findicons.com/files/icons/2389/web_icon_pack/65/bluestyle_09_star.png">		 
-				<div>Vendidos</div>
-			</a>
-			<a tablink="true" href="/reportes/top20" titulo="Top 20" class="link">
-				<img src="http://png.findicons.com/files/icons/2389/web_icon_pack/65/greenstyle_09_star.png">		 
-				<div>Top 20</div>
-			</a>
-			<a tablink="true" href="/reportes/ultimos20" titulo="Ultimos 20" class="link">
-				<img src="http://png.findicons.com/files/icons/2389/web_icon_pack/65/redstyle_09_star.png">		 
-				<div>Ultimos 20</div>
-			</a>
-			<a tablink="true" href="/reportes/novendidos" titulo="No Vendidos" class="link">
-				<img src="http://png.findicons.com/files/icons/2389/web_icon_pack/65/blackstyle_09_star.png">		          
-				<div>No vendidos</div>
-			</a>				
-		</div>
-		
-		
-		<?php
-			} 
-		?>
+	<div id="accordion">		
 		<?php 
 			if ( $_SESSION['userInfo']['rol']==1 || $_SESSION['userInfo']['rol']==2  ){
 		?>
-		<h3>Catalogos</h3>
-		<div class="submenu">
-			<a tablink="true" href="/tiendas/busqueda" titulo="Tiendas" class="link">
-				<img src="http://png.findicons.com/files/icons/2379/shopping/64/roadside_shop.png">		 
-				<div>Tiendas</div>
+		<h3>Catalogos</h3>		 
+		<div class="submenu">			
+			<a tablink="true" href="/grupos/busqueda" titulo="Grupos" class="link">
+				<img src="http://png.findicons.com/files/icons/2354/dusseldorf/32/category.png">		 
+				<div>Configuración de series.</div>
 			</a>				
 			<a tablink="true" href="/grupos/busqueda" titulo="Grupos" class="link">
 				<img src="http://png.findicons.com/files/icons/2354/dusseldorf/32/category.png">		 
-				<div>Grupos</div>
+				<div>Receta de insumo</div>
+			</a>				
+			<a tablink="true" href="/grupos/busqueda" titulo="Grupos" class="link">
+				<img src="http://png.findicons.com/files/icons/2354/dusseldorf/32/category.png">		 
+				<div>Pedido Interno</div>
+			</a>				
+			<a tablink="true" href="/tiendas/busqueda" titulo="Tiendas" class="link">
+				<img src="http://png.findicons.com/files/icons/2379/shopping/64/roadside_shop.png">		 
+				<div>Orden de Compra</div>
+			</a>				
+			<a tablink="true" href="/tiendas/busqueda" titulo="Tiendas" class="link">
+				<img src="http://png.findicons.com/files/icons/2379/shopping/64/roadside_shop.png">		 
+				<div>Compra</div>
 			</a>				
 			<a tablink="true" href="/articulos/busqueda" titulo="Articulos" class="link">
 				<img src="http://png.findicons.com/files/icons/1673/diagram_part_2/64/diagram_v2_28.png">		 
